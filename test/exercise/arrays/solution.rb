02497b344b4a6cf888ea51eac4c263bd6c8fd220
Nikolay <nickchirgin@gmail.com>
2023-01-27 12:49:44 +0300

    added array exercises

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,15 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        return array.map{ |el| el >= 0 ? max : el }
       end
 
       def search(_array, _query)
-        0
+        if _array.include?(_query)
+          return _array.find_index(_query)
+        end
+        return -1  
       end
     end
   end
